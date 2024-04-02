935a307a17a43ca5b1d70fb839314c99b194d577
Сергей Владиславович Маклецов <smak.80@yandex.ru>
2024-04-03 00:46:40 +0300

    Initial Commit

---
+++ Server/src/main/kotlin/ru/smak/chat/main.kt
@@ -0,0 +1,8 @@
+package ru.smak.chat
+
+import ru.smak.chat.net.Server
+
+fun main() {
+    val s = Server()
+    s.start()
+}
---
+++ Server/src/main/kotlin/ru/smak/chat/net/Server.kt
@@ -0,0 +1,28 @@
+package ru.smak.chat.net
+
+import java.net.ServerSocket
+import kotlin.concurrent.thread
+
+class Server(port: Int = 5104) {
+    private val serverSocket = ServerSocket(port)
+    private var stop: Boolean = false
+
+    fun start(){
+        stop = false
+        thread {
+            try {
+                while (!stop) {
+                    ConnectedClient(serverSocket.accept())
+                }
+            } catch (e: Throwable) {
+                println("Произошла непредвиденная ошибка: ${e.message}")
+            } finally {
+                serverSocket.close()
+            }
+        }
+    }
+
+    fun stop(){
+        stop = true
+    }
+}
