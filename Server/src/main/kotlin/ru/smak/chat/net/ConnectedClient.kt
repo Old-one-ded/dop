b91567ccc454b6e3307fd49971618da1ba289183
Сергей Владиславович Маклецов <smak.80@yandex.ru>
2024-04-15 09:09:02 +0300

    От 15.04

--- Server/src/main/kotlin/ru/smak/chat/net/ConnectedClient.kt
+++ Server/src/main/kotlin/ru/smak/chat/net/ConnectedClient.kt
@@ -14,6 +14,7 @@ class ConnectedClient(socket: Socket) {
     }
 
     private val communicator = Communicator(socket)
+    private var name: String? = null
 
     val isAlive: Boolean
         get() = !communicator.isStopped
@@ -30,7 +31,23 @@ class ConnectedClient(socket: Socket) {
     private fun send(cmd: Command, data: String) =
         communicator.send("$cmd:$data")
 
-    private fun process(data: String) = sendToAll(data)
+    private fun process(data: String){
+        name?.let { cName ->
+            sendToAll("$name: $data")
+        } ?: run {
+            if (clients.firstOrNull { it.name?.uppercase() == data.uppercase() } != null)
+                send(Command.SYSTEM_MESSAGE,"Выбранное имя занято. Введите другое:")
+            else {
+                if (!data.matches(Regex("^[A-ZА-Я][\\w_-]+$", RegexOption.IGNORE_CASE)))
+                    send(Command.SYSTEM_MESSAGE, "Имя должно содержать только буквы и цифры")
+                else {
+                    name = data
+                    send(Command.SYSTEM_MESSAGE, "Введите своё сообщение:")
+                    sendToAll("К нам присоединился $name!")
+                }
+            }
+        }
+    }
 
     private fun sendToAll(data: String) = clients.apply {
         removeIf { !it.isAlive }
